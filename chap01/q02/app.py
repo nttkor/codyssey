9016b97b35b223827fb00f246ae595b7d69eb212
nttkor <mpeg4@ymail.com>
2025-07-14 20:14:24 +0900

    fix 80 to 8080

--- chap01/q02/app.py
+++ chap01/q02/app.py
@@ -3,5 +3,5 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return "Hello, DevOps!" 
-# app.run(debug=False,host='0.0.0.0', port=80)  # This will run the Flask application not in debug mode 
-app.run(debug=True,host='0.0.0.0',port=80)  # This will run the Flask application in debug mode on
+# app.run(debug=False,host='0.0.0.0', port=8080)  # This will run the Flask application not in debug mode 
+app.run(debug=True,host='0.0.0.0',port=8080)  # This will run the Flask application in debug mode on
