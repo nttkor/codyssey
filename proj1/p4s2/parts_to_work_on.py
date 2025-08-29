78fb037010df5041e4408c1fec9df586aa1ddb63
nttkor <mpeg4@ymail.com>
2025-08-29 14:42:37 +0900

    refactor load_csv and get_parts_mean functions for improved data handling and calculation

--- proj1/p4s2/parts_to_work_on.py
+++ proj1/p4s2/parts_to_work_on.py
@@ -29,7 +29,7 @@ def load_and_merge_numpy():
 def load_csv():
     try:
         # 데이터만 읽어 1차원 ndarray로 만들어 준다
-        parts_name = np.loadtxt(file_path_1, delimiter=',',usecols=0)
+        parts_list = np.loadtxt(file_path_1, delimiter=',',skiprows=1,usecols=0, dtype=str)
         arr1 = np.loadtxt(file_path_1, delimiter=',', skiprows=1, usecols=1)
         arr2 = np.loadtxt(file_path_2, delimiter=',', skiprows=1, usecols=1)
         arr3 = np.loadtxt(file_path_3, delimiter=',', skiprows=1, usecols=1)
@@ -42,25 +42,26 @@ def load_csv():
         print("통합된 'parts' 배열:")
         print(parts)
         print("---------------------------------")
-        return parts
+        return parts_list, parts
     
     except FileNotFoundError as e:
         print(f"오류: 파일을 찾을 수 없습니다 - {e.filename}")
-        parts = None
-def parts_mean(parts):
+        return None, None
+def get_parts_mean(parts):
     # 'parts' 배열이 성공적으로 생성되었을 때만 실행
     if parts is not None:
         # 항목별(열별) 평균값 계산
-        column_means = np.mean(parts, axis=0)
+        column_means = np.mean(parts, axis=1)
         print("항목별 평균값:")
         print(column_means)
         print("---------------------------------")
-        
+        return column_means
         # 평균값이 50 미만인 항목의 인덱스 찾기
         indices_to_filter = np.where(column_means < 50)
-        
+def get_less_then(arr, value):
         # 조건에 맞는 열만 필터링하여 새로운 배열 생성
-        parts_to_work_on = parts[:, indices_to_filter[0]]
+        mask = arr < value
+        parts_to_work_on = arr[mask]
         print("'parts_to_work_on' 배열 (평균 < 50):")
         print(parts_to_work_on)
         print("---------------------------------")
@@ -69,8 +70,9 @@ def parts_mean(parts):
 def main():
     os.chdir('/home/mpeg4/Codyssey/proj1/p4s2')
     # parts = load_csv()
-    parts = load_csv()
-    parts_mean(parts)
+    parts_list, parts = load_csv()
+    parts_mean = get_parts_mean(parts)
+    parts_filterd = get_less_then(parts_mean, 50)
 
 if __name__ == '__main__':
     main()
