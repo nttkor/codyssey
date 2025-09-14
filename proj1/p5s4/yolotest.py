28df61a46a026e15d7d985cee39fe59804856c99
nttkor <mpeg4@ymail.com>
2025-09-15 01:12:09 +0900

    yolo test

--- proj1/p5s4/yolotest.py
+++ proj1/p5s4/yolotest.py
@@ -1,5 +1,7 @@
+import os
 from ultralytics import YOLO
-
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+print(os.getcwd())
 # 1) 사전학습 모델 로드 (경량→고속: yolov8n.pt / 정확도↑: yolov8s/m/l/x.pt)
 model = YOLO("yolov8n.pt")
 
@@ -7,11 +9,13 @@ model = YOLO("yolov8n.pt")
 # classes=[0] 이 COCO에서 person 클래스 인덱스
 # conf=0.25~0.5 사이에서 환경에 맞게 조절 (우주복/특수조명이라면 0.25~0.35 권장)
 results = model.predict(
-    source="your_input.mp4",  # "image.jpg", 0(웹캠), "folder/", "rtsp://..." 모두 가능
+    source= "cctv\cctv-1.jpg" ,
+    # 'proj1\p5s4\cctv\cctv-1.jpg' 
+    #"your_input.mp4",  # "image.jpg", 0(웹캠), "folder/", "rtsp://..." 모두 가능
     classes=[0],
     conf=0.3,
     iou=0.5,     # 박스 중복 억제
-    imgsz=640    # 640 기본, 멀리 있는 사람은 720~960도 시도
+    imgsz=1024    # 640 기본, 멀리 있는 사람은 720~960도 시도
 )
 
 # 3) 시각화/저장 (Ultralytics가 자동 저장 옵션도 제공)
