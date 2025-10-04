507609bb9844df9f9533ba8615b0b929e5998f37
nttkor <mpeg4@ymail.com>
2025-10-04 20:53:31 +0900

    kaggle 데이터분석

--- proj2/c4q1_ex.py
+++ proj2/c4q1_ex.py
@@ -1,6 +1,7 @@
-import pandas as pd
-test = pd.read_csv('data/test.csv')
-train = pd.read_csv('data/train.csv')
-print(test)
-print(train)
+# import pandas as pd
+# test = pd.read_csv('data/test.csv')
+# train = pd.read_csv('data/train.csv')
+# print(test)
+# print(train)
 print("hello")
+print(__file__)
