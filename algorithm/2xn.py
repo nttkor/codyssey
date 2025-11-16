ef1516b7073a85a07413609a41e8a7e7c5784e8a
nttkor <mpeg4@ymail.com>
2025-11-16 12:48:13 +0900

    passwd

--- algorithm/2xn.py
+++ algorithm/2xn.py
@@ -9,12 +9,13 @@ def fill_2XN(n):
 print(fill_2XN(8))
 memo={}
 def dfs(i):
+    if i in memo:
+        return memo[i]
     if i==0:
         return 1
     if i<0:
         return 0
-    if i in memo:
-        return memo[i]
+    
     memo[i] = dfs(i-1)+2*dfs(i-2)
     return memo[i]
 print(dfs(8))
